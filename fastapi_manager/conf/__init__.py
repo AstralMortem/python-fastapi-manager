60951202bcdb88f271a184b71442bd2c50ace33d
AstralMortem <chaliukvladyslav@gmail.com>
2024-08-30 11:58:53 +0300

    add settings manager

---
+++ fastapi_manager/conf/__init__.py
@@ -0,0 +1,21 @@
+import dynaconf
+from pathlib import Path
+import global_settings
+import os
+
+GLOBAL_SETTINGS_PATH = Path(global_settings.__file__).absolute()
+SETTINGS_ENV = "FATAPI_SETTINGS"
+
+# add parsers to convert string to pathlib in configs
+dynaconf.utils.parse_conf.converters["@path"] = lambda x: Path(x).absolute()
+
+settings = dynaconf.Dynaconf(
+    environments=True,
+    load_dotenv=True,
+    settings_files=[
+        str(GLOBAL_SETTINGS_PATH),
+        str(os.environ.get(SETTINGS_ENV, None)),
+    ],
+    merge_enabled=False,
+    envvar_prefix="FASTAPI",
+)
